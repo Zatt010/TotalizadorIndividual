fc44b0f72522930a32efb5cff72a23d06c2a3a27
Zatt010 <afuheho1@gmail.com>
2023-08-25 10:56:35 -0400

    Modificacion prueba CA

--- src/Bisiesto.spec.js
+++ src/Bisiesto.spec.js
@@ -2,7 +2,10 @@ import Calcular from "./Bisiesto.js";
 
 describe("Calcular Total", () => {
   it("Esta bien CA:", () => {
-    expect(Calcular(20,3,8.25)).toEqual(64.95);
+    expect(Calcular(20, 3, 8.25)).toEqual({
+      total_con_impuesto: 64.95,
+      descuento: 0.00,
+    });
   });
   it("Esta bien TX:", () => {
     expect(Calcular(20,3,6.25)).toEqual(63.75);
